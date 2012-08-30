a987b80ff16fb6196b845940549669f4340b0c40
Peter Ansell <p_ansell@yahoo.com>
2012-08-30 15:12:36 +1000

    add podd-prototype to test out some methodologies

---
+++ podd-prototype/src/main/java/com/example/podd/prototype/PoddPrototype.java
@@ -0,0 +1,16 @@
+package com.example.podd.prototype;
+
+public class PoddPrototype
+{
+    public static void main(String[] args)
+    {
+        System.out.println("PODD Prototype tester");
+    }
+    
+    
+    public PoddPrototype()
+    {
+        // TODO Auto-generated constructor stub
+    }
+    
+}
