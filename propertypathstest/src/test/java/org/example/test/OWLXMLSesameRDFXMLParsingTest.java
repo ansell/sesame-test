3eea413c416892963757be626583a07c1b1fd79b
Peter Ansell <p_ansell@yahoo.com>
2012-11-01 10:54:53 +1000

    add test for primer.owlxml.xml to verify that it always fails to be parsed by sesame
    reason for this is that there are some strange results in owlapi where it appears that the rio RDFXML parser successfully parses this owl/xml file that should not validate as an RDF/XML file

---
+++ propertypathstest/src/test/java/org/example/test/OWLXMLSesameRDFXMLParsingTest.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package org.example.test;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openrdf.model.Statement;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParseException;
+
+/**
+ * @author Peter Ansell p_ansell@yahoo.com
+ * 
+ */
+public class OWLXMLSesameRDFXMLParsingTest extends AbstractSesameTest
+{
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.example.test.AbstractSesameTest#setUp()
+     */
+    @Override
+    @Before
+    public void setUp() throws Exception
+    {
+        super.setUp();
+    }
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.example.test.AbstractSesameTest#tearDown()
+     */
+    @Override
+    @After
+    public void tearDown() throws Exception
+    {
+        super.tearDown();
+    }
+    
+    @Test
+    public final void testOWLXMLParsingUsingRDFXMLParser() throws RDFParseException, RepositoryException, IOException
+    {
+        AtomicInteger failures = new AtomicInteger(0);
+        for(int i = 0; i < 100000; i++)
+        {
+            try
+            {
+                this.getTestRepositoryConnection().add(this.getClass().getResourceAsStream("/primer.owlxml.xml"), "",
+                        RDFFormat.RDFXML);
+                this.getTestRepositoryConnection().commit();
+                
+                // If the parse succeeds for any reason verify that there are not any generated triples in the repository
+                Assert.assertEquals(0, this.getTestRepositoryConnection().size());
+                
+                this.getTestRepositoryConnection().clear();
+            }
+            catch(RDFParseException rdfpe)
+            {
+                failures.incrementAndGet();
+            }
+        }        
+        
+        Assert.assertEquals(100000, failures.get());
+    }
+    
+}
